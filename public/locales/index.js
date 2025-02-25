342f1fe9a9004698f39bde8e0781e47b8d966b09
Amr3011 <amroosamafakharany@gmail.com>
2025-02-25 21:23:27 +0200

    `Update import paths in public/locales/index.js`

--- public/locales/index.js
+++ public/locales/index.js
@@ -1,2 +1,2 @@
-export { default as testEn } from "../locals/en/test.json";
-export { default as testAr } from "../locals/ar/test.json";
+export { default as testEn } from "../locales/en/test.json";
+export { default as testAr } from "../locales/ar/test.json";
