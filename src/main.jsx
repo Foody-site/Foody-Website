657bf9370d91c2a60ef9e127441e879f8bccdff7
Amr3011 <amroosamafakharany@gmail.com>
2025-02-26 19:04:16 +0200

    `Added BrowserRouter and AppRouter to the application`

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { createRoot } from "react-dom/client";
 import "./i18n"; // استيراد الترجمة
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
