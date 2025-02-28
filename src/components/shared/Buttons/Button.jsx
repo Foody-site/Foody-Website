de5e058dbd6a7c4967c11c5d70da6b5cfe16a7fd
Amr3011 <amroosamafakharany@gmail.com>
2025-02-28 17:36:47 +0200

    `Refactor Login component to include social login buttons and additional features`

--- src/components/shared/Buttons/Button.jsx
+++ src/components/shared/Buttons/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, onClick, className = '' }) => {
   return (
     <button
       onClick={onClick}
-      className={`btn bg-primary-1 hover:bg-hover_primary-1 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition duration-300 ${className}`}
+      className={`w-full p-3 text-white font-semibold rounded-md  ${className}`}
     >
       {label}
     </button>
