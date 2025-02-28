5bddbe6157206258d1a62518fd289c57a7b6bdf0
Amr3011 <amroosamafakharany@gmail.com>
2025-02-28 14:19:38 +0200

    `Remove 'no-animation' class from Button component's className`

--- src/components/shared/Buttons/Button.jsx
+++ src/components/shared/Buttons/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, onClick, className = '' }) => {
   return (
     <button
       onClick={onClick}
-      className={`btn no-animation bg-primary-1 hover:bg-hover_primary-1 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition duration-300 ${className}`}
+      className={`btn bg-primary-1 hover:bg-hover_primary-1 text-white font-semibold py-2 px-6 rounded-lg shadow-md transition duration-300 ${className}`}
     >
       {label}
     </button>
