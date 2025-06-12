9643ea240d0356d0f28cbd87d47a1545e60107c2
Amr3011 <amroosamafakharany@gmail.com>
2025-06-12 18:05:59 +0300

    `Refactored DeliveryForm and related components to improve performance and functionality`

--- src/components/shared/inputs/DeliveryApps.jsx
+++ src/components/shared/inputs/DeliveryApps.jsx
@@ -1,30 +1,7 @@
 import React, { useState } from "react";
 import Inputs from "./Inputs";
 
-const DeliveryApps = () => {
-  const [apps, setApps] = useState([
-    { name: "keeta", label: "كيتا", checked: false },
-    { name: "jahez", label: "جاهز", checked: false },
-    { name: "hungerStation", label: "هنقرستيشن", checked: false },
-    { name: "mrsool", label: "مرسول", checked: false },
-    { name: "noon", label: "نون", checked: false },
-    { name: "toyou", label: "تويو", checked: false },
-    { name: "theChefz", label: "ذا شيفز", checked: false },
-    { name: "uber", label: "اوبر", checked: false },
-    { name: "mrMandoob", label: "مستر مندوب", checked: false },
-    { name: "other", label: "أخرى", checked: false },
-    { name: "careem", label: "كريم", checked: false },
-    { name: "shgardi", label: "شقر دي", checked: false },
-  ]);
-
-  const handleCheckboxChange = (id) => {
-    setApps(
-      apps.map((app) =>
-        app.name === id ? { ...app, checked: !app.checked } : app
-      )
-    );
-  };
-
+const DeliveryApps = ({ apps, onCheckboxChange }) => {
   return (
     <div className="w-full">
       <h2 className="text-2xl font-bold text-right mb-6">تطبيقات التوصيل</h2>
@@ -32,7 +9,6 @@ const DeliveryApps = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {apps.map((app) => (
             <div key={app.name} className="flex flex-col items-end">
-              {/* Checkbox and Label */}
               <div className="flex items-center mb-2">
                 <span className="text-gray-700 text-lg font-medium">
                   {app.label}
@@ -40,12 +16,11 @@ const DeliveryApps = () => {
                 <input
                   type="checkbox"
                   checked={app.checked}
-                  onChange={() => handleCheckboxChange(app.name)}
+                  onChange={() => onCheckboxChange(app.name)}
                   className="w-6 h-6 ml-2 bg-gray-100 border-2 border-primary-1 accent-primary-1 rounded cursor-pointer focus:outline-none"
                 />
               </div>
 
-              {/* Input Field */}
               <Inputs
                 type="text"
                 name={app.name}
