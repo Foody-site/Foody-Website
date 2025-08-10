204752d3563e1607068cfe9481ab2bc777277f3d
Amr3011 <amroosamafakharany@gmail.com>
2025-08-10 22:59:15 +0300

    Refactor configuration files: simplify lint script, clean up vercel.json, and remove unused build options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // Suppress certain warnings
-        if (warning.code === "UNUSED_EXTERNAL_IMPORT") return;
-        warn(warning);
-      },
-    },
-  },
-  esbuild: {
-    logOverride: { "this-is-undefined-in-esm": "silent" },
-  },
 });
